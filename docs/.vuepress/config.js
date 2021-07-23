3d269af0a62e9fab9ebbafb90b0d1f990bce7b59
荣顶 <wangrongding@qq.com>
2021-07-23 14:31:38 +0800

    feat: 完善个人简介

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,6 +16,28 @@ module.exports = {
                 content: "width=device-width,initial-scale=1,user-scalable=no",
             },
         ],
+        [
+            "meta",
+            {
+                name: "description",
+                content:
+                    "汪荣顶,汪荣顶的个人博客,在这里,我会分享一些前端开发知识.",
+            },
+        ],
+        [
+            "meta",
+            {
+                name: "keywords",
+                content: "汪荣顶,荣顶,wangrongding,rongding",
+            },
+        ],
+        [
+            "meta",
+            {
+                name: "author",
+                content: "汪荣顶,wangrongding@qq.com,htpps://www.fedtop.com",
+            },
+        ],
     ],
     markdown: {
         lineNumbers: true,
