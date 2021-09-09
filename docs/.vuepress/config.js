754b6e214e1f0c9c8f73c0dae46d88c5be3025a6
荣顶 <wangrongding@qq.com>
2021-09-09 14:19:38 +0800

    feat: 添加加群浮窗

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     themeConfig,
     theme: "reco",
     base: "/",
-    title: "汪荣顶的个人博客",
+    title: "荣顶 - 个人博客",
     description: "Hey,you are the only one worthwhile!",
     dest: "public",
     head: [
@@ -43,5 +43,41 @@ module.exports = {
     markdown: {
         lineNumbers: true,
     },
-    plugins: ["@vuepress/medium-zoom", "flowchart"],
+    plugins: [
+        "@vuepress/medium-zoom",
+        "flowchart",
+        [
+            "@vuepress-reco/vuepress-plugin-bulletin-popover",
+            {
+                width: "300px", // 默认 260px
+                title: "消息提示",
+                body: [
+                    {
+                        type: "title",
+                        content:
+                            "如果你也非常热爱前端相关技术！欢迎加入我的小密圈~ 🦄",
+                        style:
+                            "text-align:left;font-size:16px;line-height:30px;",
+                    },
+                    {
+                        type: "image",
+                        src:
+                            "https://gitee.com/wangrongding/image-house/raw/master/2021-9-9/1631165421779-%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20210818170639.jpg",
+                    },
+                    {
+                        type: "text",
+                        content:
+                            "微信扫描二维码，拉你进前端技术交流群，每日精选好文~ 😉",
+                    },
+                ],
+                footer: [
+                    /* {
+                        type: "button",
+                        text: "打赏",
+                        link: "/donate",
+                    }, */
+                ],
+            },
+        ],
+    ],
 };
