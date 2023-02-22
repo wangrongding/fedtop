eb7daa427193fe7f4cf9a0e1abd262fa31839cab
荣顶 <wangrongding@qq.com>
2023-02-22 09:43:15 +0800

    fix: 调整文章时间格式

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -6,7 +6,12 @@ type Props = {
 
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+  return (
+    <time dateTime={dateString}>
+      {/* {format(date, 'LLLL	d, yyyy')} */}
+      {format(new Date(date), 'yyyy-MM-dd')}
+    </time>
+  )
 }
 
 export default DateFormatter
