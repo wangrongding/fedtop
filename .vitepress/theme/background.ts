9dcc45697b722561f0db911264fca4c6aaacf58a
荣顶 <wangrongding@qq.com>
2023-08-09 11:36:41 +0800

    fix: 修复背景样式导致的文章目录无法固定的问题

--- .vitepress/theme/background.ts
+++ .vitepress/theme/background.ts
@@ -4,5 +4,11 @@ function dynamicBackground() {
   div.style.cssText =
     "position:fixed;top:0;left:0;z-index:0;width:100%;height:100%;pointer-events:none;background: url('/bg.svg') center/cover no-repeat;"
   document.body.appendChild(div)
+  // 创建模糊层
+  const blurLayer = document.createElement('div')
+  blurLayer.className = 'dynamic-background-blur'
+  blurLayer.style.cssText =
+    'position:fixed;top:0;left:0;z-index:0;width:100%;height:100%;pointer-events:none;background:rgba(0,0,0,0);backdrop-filter: blur(8px)'
+  document.body.appendChild(blurLayer)
 }
 export default dynamicBackground
